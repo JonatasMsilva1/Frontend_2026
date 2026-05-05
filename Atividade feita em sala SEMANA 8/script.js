5f6942633ec7fad370bf019809b583a4c9dd46a6
Jonatas Marques Da Silva <jonatas.marquesda.silva10@gmail.com>
2026-05-05 20:35:37 -0300

    Create script.js

---
+++ Atividade feita em sala SEMANA 8/script.js
@@ -0,0 +1,12 @@
+const taskInput = document.getElementById('taskInput')
+const taskList = document.getElementById('taskList')
+
+function addTask(){
+  const textInput = taskInput.value.trim()
+  if(textInput != ''){
+    const newItem = document.createElement('li')
+    newItem.innerHTML = `<span>${textInput}</span>`
+    taskList.appendChild(newItem)
+  }
+  
+}
